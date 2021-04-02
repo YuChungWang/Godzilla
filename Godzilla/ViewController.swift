5a34e985667a25d918ed4eb415fabad63924f2b0
charlie.wang <charliewang324@gmail.com>
2021-04-02 16:10:29 +0800

    Initial Commit

---
+++ Godzilla/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Godzilla
+//
+//  Created by 王昱中 on 2021/4/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
